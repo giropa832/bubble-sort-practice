2f17606dbd8eaedeceec559efa3993b1781ab50e
Jerónimo Palacios <giropa832@gmail.com>
2023-01-07 20:27:02 +0100

    It performs the bubble sort but doesn't match the test

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,18 +1,26 @@
 
 function bubbleSort(arr) {
+  let swap = false;
 
-    // Iterate through the array
+  // Iterate through the array
+  do {
+    swap = false;
+    for (let i = 0; i < arr.length; i++) {
 
       // If the current value is greater than its neighbor to the right
+      if (arr[i] > arr[i + 1]) {
+
         // Swap those values
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swap = true;
+      }
+    }
+    console.log(arr.join(","));
+  } while (swap);
+  // If you get to the end of the array and no swaps have occurred, return
+  return arr;
 
-        // Do not move this console.log
-        console.log(arr.join(","));
-
-    // If you get to the end of the array and no swaps have occurred, return
-
-    // Otherwise, repeat from the beginning
-
+  // Otherwise, repeat from the beginning
 }
 
 module.exports = bubbleSort;
